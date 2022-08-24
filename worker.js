a860bfc4a4c3acb3b7468a62f1da9c458e8df295
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 14:08:15 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,11 @@
+import camelcaseKeys from 'camelcase-keys'
+
+export default {
+  fetch: async req => {
+    const { pathname } = new URL(req.url)
+    console.log('https:/' + pathname)
+    const data = await fetch('https:/' + pathname, req).then(res => res.json())
+    return new Response(JSON.stringify(flatten(camelcaseKeys(data, { deep: true })), null, 2), { headers: { 'content-type': 'application/json' }})
+  }
+}
+
