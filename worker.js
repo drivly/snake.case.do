fc702ad5ac7102f80c0435423ce35e40d4af4365
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 14:27:14 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,8 +2,8 @@ import camelcaseKeys from 'camelcase-keys'
 
 export default {
   fetch: async req => {
-    const { pathname } = new URL(req.url)
-    console.log('https:/' + pathname)
+    const { pathname, search } = new URL(req.url)
+    console.log('https:/' + pathname + search)
     const data = await fetch('https:/' + pathname, req).then(res => res.json())
     return new Response(JSON.stringify(camelcaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
   }
