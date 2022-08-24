b92ed525d90b9ac81ee2d9a14f6e7cf7e10842cc
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 14:56:03 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
-import camelcaseKeys from 'camelcase-keys'
+import snakecaseKeys from 'snakecase-keys'
 
 export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
     const data = await fetch('https:/' + pathname + search, req).then(res => res.json())
-    return new Response(JSON.stringify(camelcaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify(snakecaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
   }
 }
 
