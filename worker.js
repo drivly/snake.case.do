2d904ab8c42750485f17f4f80941c55320e8641f
Nathan Clevenger <nateclev@gmail.com>
2022-09-07 06:22:33 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
     const data = await fetch('https:/' + pathname + search, req).then(res => res.json())
-    return new Response(JSON.stringify(snakecaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify(snakecaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   }
 }
 
